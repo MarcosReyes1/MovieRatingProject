3d0d6f716fd8d37029cb38eb3d2deac2b2972d04
Marcos <reyes.marcos1@outlook.com>
2025-06-25 17:00:35 -0700

    Fetch guest login id

---
+++ src/pages/auth/mutation.ts
@@ -0,0 +1,12 @@
+export const mutationLogin = async () => {
+    const res = await fetch(
+        "https://api.themoviedb.org/3/authentication/guest_session/new",
+        {
+            headers: {
+                Authorization: 
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWM1NzAyZWYzNTEwZjgxZmFmMjI2MjY2NDk4ODVlMiIsIm5iZiI6MTc1MDg2Nzc3Ni4yMjcsInN1YiI6IjY4NWMxZjQwZmFjZDEyNGI0YjE3MDgxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AZHjMmG51PT57ggkwJ9NtvLPc-cMrzKbdtPRfsGGXaA"
+            },
+        }
+    );
+    return (await res.json());
+};
